e14ec8a3b4690e991330059178c76f07a4acc663
Intellipaat Cloud <intellipaatcloud@gmail.com>
2019-07-03 02:58:33 +0000

    Coomit for new dev branch

--- helloworld.java
+++ helloworld.java
@@ -3,10 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-        System.out.println("Hello, Change2");
-        System.out.println("Hello, Change3");
-        System.out.println("Hello, Change4");
-        System.out.println("Hello, Change5");
+        System.out.println("Hello dev branch");
 	// change #2
     }
 
